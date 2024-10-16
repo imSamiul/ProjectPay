a43368bb3be16fd48c7110fd5116d7e075891b1f
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-10-17 01:55:49 +0600

    Refactor PaymentListTable component to include sorting and filtering functionality

--- src/components/table/PaymentListTable.tsx
+++ src/components/table/PaymentListTable.tsx
@@ -2,16 +2,20 @@ import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
 } from "@tanstack/react-table";
 import { PaymentType } from "../../types/paymentType";
 import { useState } from "react";
+import { LuArrowDownUp } from "react-icons/lu";
 
 const columnHelper = createColumnHelper<PaymentType>();
 const columns = [
   columnHelper.accessor("paymentDate", {
     header: "Payment Date",
-    cell: (info) => info.getValue(),
+    cell: (info) => new Date(info.getValue()).toLocaleDateString("en-GB"),
   }),
   columnHelper.accessor("paymentAmount", {
     header: "Payment Amount",
@@ -33,31 +37,53 @@ const columns = [
 ];
 
 function PaymentListTable({ data }: { data: PaymentType[] }) {
-  const [sorting, setSorting] = useState([]);
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [globalFilter, setGlobalFilter] = useState("");
   const table = useReactTable({
     data,
     columns,
     state: {
       sorting,
+      globalFilter,
     },
     getCoreRowModel: getCoreRowModel(),
     onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
   });
-  console.log(table.getRowModel());
+  //   console.log(table.getRowModel());
 
   return (
     <div>
+      <div className="p-4">
+        <input
+          value={globalFilter ?? ""}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Search ... "
+          className="input input-bordered"
+        />
+      </div>
+
       <table className="table">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
                 <th key={header.index}>
-                  <div>
+                  <div
+                    className={`flex items-center gap-2 ${
+                      header.column.getCanSort()
+                        ? `cursor-pointer select-none flex items-center`
+                        : ""
+                    }`}
+                    onClick={header.column.getToggleSortingHandler()}
+                  >
                     {flexRender(
                       header.column.columnDef.header,
                       header.getContext(),
                     )}
+                    <LuArrowDownUp />
                   </div>
                 </th>
               ))}
